f95bc4e6fb3973e20250cefe1a4460ed3530e78e
amirhossein-github <amirhossein.business.developer@gmail.com>
2024-07-22 16:24:50 +0330

    refactor: update code structure
    The previous structure was not correct and principled, and in this structure I use a comment and used arrays in the appropriate place instead of objects.

--- course-exercises/course-1/session-5/assets/script.js
+++ course-exercises/course-1/session-5/assets/script.js
@@ -1,49 +1,67 @@
+/*
+object school include
+school >  classes : []
+
+each classes include
+classes > students : []
+
+each students include
+list of student [{1}, {2}, {3}]
+*/
 const school = {
-    classA : {
-        studnet1: {
-            name: 'Behdakht',
-            family: 'Tawakkel',
-            phone: '02603460732',
-            address: 'Golestan, Ebadi Street, Shidasab Building',
-            GAD: 19.5
+    classes: [
+        {
+            name: 'ClassA',
+            students: [
+                {
+                    name: 'Behdakht',
+                    family: 'Tawakkel',
+                    phone: '02603460732',
+                    address: 'Golestan, Ebadi Street, Shidasab Building',
+                    GAD: 19.5
+                },
+                {
+                    name: 'Khorhad',
+                    family: 'Jannati',
+                    phone: '02651148054',
+                    address: 'East Azarbaijan Province, Frouten Street, Polad Building',
+                    GAD: 15
+                },
+                {
+                    name: 'Hafez',
+                    family: 'Payor',
+                    phone: '02625927307',
+                    address: 'Mazandaran Province, Muhaddi Street, Saba Building',
+                    GAD: 10
+                }
+            ]
         },
-        studnet2: {
-            name: 'Khorhad',
-            family: 'Jannati',
-            phone: '02651148054',
-            address: 'East Azarbaijan Province, Frouten Street, Polad Building',
-            GAD: 15
-        },
-        studnet3: {
-            name: 'Hafez',
-            family: 'Payor',
-            phone: '02625927307',
-            address: 'Mazandaran Province, Muhaddi Street, Saba Building',
-            GAD: 10
-        },
-    },
-    classB : {
-        studnet1: {
-            name: 'Leith',
-            family: 'Taleghani',
-            phone: '02661718418',
-            address: 'Khuzestan Province, Qanei Street, Ayaz Building',
-            GAD: 20
-        },
-        studnet2: {
-            name: 'Sika',
-            family: 'Ebtekari',
-            phone: '02652931417',
-            address: 'Isfahan, Aali Street, Niro building',
-            GAD: 13
-        },
-        studnet3: {
-            name: 'Tahamtan',
-            family: 'Shirazi',
-            phone: '02607881184',
-            address: 'Fars Province, Ashtari Street, Artadekht Building',
-            GAD: 18.75
+        {
+            name: 'ClassB',
+            students: [
+                {
+                    name: 'Leith',
+                    family: 'Taleghani',
+                    phone: '02661718418',
+                    address: 'Khuzestan Province, Qanei Street, Ayaz Building',
+                    GAD: 20
+                },
+                {
+                    name: 'Sika',
+                    family: 'Ebtekari',
+                    phone: '02652931417',
+                    address: 'Isfahan, Aali Street, Niro building',
+                    GAD: 13
+                },
+                {
+                    name: 'Tahamtan',
+                    family: 'Shirazi',
+                    phone: '02607881184',
+                    address: 'Fars Province, Ashtari Street, Artadekht Building',
+                    GAD: 18.75
+                }
+            ]
         }
-    },
+    ]
 }
 console.log(school)
