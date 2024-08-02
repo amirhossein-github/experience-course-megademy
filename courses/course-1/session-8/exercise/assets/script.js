4d5af1fefd3f02ff526151e14cfd72d2d889256d
amirhossein-github <amirhossein.business.developer@gmail.com>
2024-08-03 02:07:54 +0330

    feat: exercise session - 8
    I solved the question of the text searchedr and added the code to the file.

--- courses/course-1/session-8/exercise/assets/script.js
+++ courses/course-1/session-8/exercise/assets/script.js
@@ -16,3 +16,28 @@ console.log(`${num1} / ${num2} = `, calc = num1 / num2)
 console.groupEnd()
 
 //----------------------------------------------------------------------------------------------------
+
+console.warn("Question 4: Text Searcher 🔎")
+// Section (4)
+// Search (vojod Darad Nadarad)
+string = '...HARCHI...', target = 'x'
+
+// Your code
+console.group("Section Text Searcher")
+// 1) Simple If and Else statment
+if (string.includes(target)) {
+    console.log(`The desired text (${target}) exist.`)
+} else {
+    console.log(`The desired text (${target}) does not exist.`)
+}
+
+// 2) console.assert()
+console.assert(string.includes(target) === true, `The desired text (${target}) does not exist.`) // If target is not found
+console.assert(string.includes(target) === false, `The desired text (${target}) exists.`) // If target is found
+
+// 3) Short If, condition ? true : false
+console.log(`The desired text (${target}) ` + String((string.includes(target)) ? 'exist' : 'does not exist'))
+console.groupEnd()
+
+//----------------------------------------------------------------------------------------------------
+// flag{aGVoZWhlaCA6KQ==}
