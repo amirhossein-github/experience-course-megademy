16bf5892e7c2ddd05a1aacee9b94f9355e2177dc
amirhossein-github <amirhossein.business.developer@gmail.com>
2024-08-03 02:07:20 +0330

    feat: exercise session - 8
    I solved the question of the calculator and added the code to the file.

---
+++ courses/course-1/session-8/exercise/assets/script.js
@@ -0,0 +1,18 @@
+console.clear()
+
+//----------------------------------------------------------------------------------------------------
+
+console.warn("Question 3: Calculator 🧮")
+// Section (3)
+// Calculator
+let calc = 0, num1 = 4, num2 = 5
+
+// Your code
+console.group("Section Calculator")
+console.log(`${num1} + ${num2} = `, calc = num1 + num2)
+console.log(`${num1} - ${num2} = `, calc = num1 - num2)
+console.log(`${num1} * ${num2} = `, calc = num1 * num2)
+console.log(`${num1} / ${num2} = `, calc = num1 / num2)
+console.groupEnd()
+
+//----------------------------------------------------------------------------------------------------
